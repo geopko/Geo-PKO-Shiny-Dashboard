e8e9dada9a3afe0884a49f982858aa715cefefa4
Nguyen Ha <hatnguyen.267@gmail.com>
2021-05-12 16:58:43 +0200

    Fixing TCCmap issues; this version has been deployed to Shiny

--- data_wrangling.R
+++ data_wrangling.R
@@ -9,8 +9,8 @@ library(readxl)
 library(lubridate)
 
 #### dataset with ISO codes for mapmaker ####
-geopko_raw <- read_xlsx("Geo_PKO_v.2.1.xlsx", col_types = c("text"))
-missingyears <- read_xlsx("missingyears.xlsx", col_types = c("text"))
+geopko_raw <- read_xlsx("data/Geo_PKO_v.2.1.xlsx", col_types = c("text"))
+missingyears <- read_xlsx("data/missingyears.xlsx", col_types = c("text"))
 
 # filtering for any republished maps to remove incorrect data
 geopkofilter <- geopko_raw %>%
@@ -28,10 +28,14 @@ map_df <- geopko %>%
                    unmo.dummy, unmo.dummy)), as.numeric) %>% 
   mutate(hq=as.factor(hq))
 
-#appending ISO code to the dataset
+####appending ISO code to the dataset for the static map makers####
 ISO_df <- codelist %>% select(cown, iso3c) %>% mutate_all(as.character)
 
 map_df <- left_join(map_df, ISO_df, by=c("cow_code"="cown"))
+
+# Make sure the system locale is English before performing this step. 
+# Sys.setlocale("LC_MESSAGES", 'en_GB.UTF-8')
+# Sys.setenv(LANG = "en_US.UTF-8")
 
 map_df <- map_df %>%
   mutate(iso3c = case_when(country == "Ethiopia/Eritrea" ~ "ETH, ERI",
@@ -45,9 +49,10 @@ map_df <- map_df %>%
   unite(joined_date, c("year","MonthName"), sep=": ", remove=FALSE) %>%
   unite(timepoint, c("year","MonthName"), sep=" ", remove=FALSE)
 
-write_excel_csv(map_df, "Geo_PKO_v2.1_ISO3.csv")
+write_excel_csv(map_df, "data/Geo_PKO_v.2.1_ISO3.csv") # saving files
+writexl::write_xlsx(map_df, "data/Geo_PKO_v.2.1_ISO3.xlsx")
 
-#### file for leaflet ####
+#### map files for leaflet ####
 
 geopko2 <-geopko %>% mutate_at(vars(he.sup, inf.no, res.no, 
                                     avia,longitude, latitude), as.numeric) %>%
@@ -75,6 +80,7 @@ FrontmapData <- geopko2 %>%
   distinct() 
 
 write_excel_csv(FrontmapData, "FrontMap.csv")
+write_excel_csv(FrontmapData, "FrontMap.xlsx")
 
 #### TCC dataframe (second map) ####
 TCCmapData<-geopko2 %>% select(source:location, latitude, longitude,
@@ -82,24 +88,29 @@ TCCmapData<-geopko2 %>% select(source:location, latitude, longitude,
   pivot_longer(nameoftcc_1:notroopspertcc_17,
                names_to=c(".value", "TCC_id"),
                names_sep="_") %>%
-  filter(!is.na(nameoftcc)) %>%  #dropping empty tcc name cells
+  #dropping empty no.troops cells 
+  filter(notroopspertcc != "NA") %>%
   mutate_at(vars(notroopspertcc), as.numeric) %>%
+  mutate(nameoftcc = tolower(nameoftcc)) %>% 
   group_by(mission, year, location, latitude, longitude, nameoftcc) %>%
-  summarise(count.per.tcc.year=as.character(max(notroopspertcc))) %>%
+  summarise(count.per.tcc.year=as.character(max(notroopspertcc), na.rm = TRUE)) %>%
   mutate(nameoftcc = str_to_title(nameoftcc),
          nameoftcc = case_when(nameoftcc == "Uk" ~ "UK", 
+                               nameoftcc == "U.k." ~ "UK",
                                nameoftcc == "Cote D'ivoire" ~ "Cote d'Ivoire", 
                                nameoftcc == "Usa" ~ "USA", 
                                nameoftcc == "Caricom" ~ "CARICOM",
                                nameoftcc == "Korea (Rok)" ~ "Korea (ROK)",
+                               nameoftcc == "Unknown" ~ "TCC name unknown",
                                TRUE ~ nameoftcc)) %>%
   mutate(count.per.tcc.year=ifelse(is.na(count.per.tcc.year), "size unknown", count.per.tcc.year),
          single.tcc=paste0(nameoftcc, " (",count.per.tcc.year,(")"))) %>%
-  add_count(year, location, name="no_tcc")%>%
-  group_by(mission, year, location, latitude, longitude, no_tcc) %>%
+  add_count(year, location, name="No.TCC")%>%
+  group_by(mission, year, location, latitude, longitude, No.TCC) %>%
   summarise(year.overview = str_c(single.tcc, collapse=", ")) 
 
-write_excel_csv(TCCmapData, "TCCMap.csv")
+write_excel_csv(TCCmapData, "data/TCCMap.csv")
+writexl::write_xlsx(TCCmapData, "data/TCCMap.xlsx")
 
 ####Troop type####
 geopko2 <-geopko %>% 
@@ -128,8 +139,10 @@ TTmapData <- geopko2 %>%
   distinct()%>%
   drop_na(Infantry)
 
-write_excel_csv(TTmapData, "TTMap.csv")
+write_excel_csv(TTmapData, "data/TTMap.csv")
+write_excel_csv(TTmapData, "data/TTMap.xlsx")
 
+### OLD SCRIPT SEGMENTS, FOR TESTING PURPOSE 
 library(readxl)
 library(readr)
 geopko2 <- readxl::read_xlsx("Geo_PKO_v.2.1.xlsx", col_types="text")
